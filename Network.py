bfc6eef4b6ad84bf75d2b248bc981c092dafe262
gwjm <gregor@the-macdonalds.net>
2023-11-01 23:00:37 -0700

    Added import statements to network.py

---
+++ Network.py
@@ -0,0 +1,9 @@
+#File meant to be changed to use whatever framework/library needed. 
+
+#TODO: Implement tensorflow and pytorch based network classes
+
+import tensorflow as tf 
+import keras 
+import torch.nn as nn 
+import torch.functional as F 
+import torch.optim 
